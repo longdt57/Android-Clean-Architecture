19f5cced0a974bb775b55f68d82308c10bf7086a
logan <longdt57@gmail.com>
2025-06-06 21:31:43 +0700

    Add Intent Extension

--- core/core-ktx/src/main/java/leegroup/module/core/extensions/IntentExt.kt
+++ core/core-ktx/src/main/java/leegroup/module/core/extensions/IntentExt.kt
@@ -2,7 +2,9 @@ package leegroup.module.core.extensions
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Parcelable
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 
 fun Intent.getExtraStreamUri(): Uri? = try {
     extras?.getParcelable(Intent.EXTRA_STREAM)
@@ -13,4 +15,25 @@ fun Intent.getExtraStreamUri(): Uri? = try {
 
 fun Intent.clearExtraStreamUri() {
     removeExtra(Intent.EXTRA_STREAM)
+}
+
+inline fun <reified T : Parcelable> Intent.getParcelable(key: String = T::class.java.simpleName): T? {
+    return if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(key, T::class.java)
+    } else {
+        getParcelableExtra(key)
+    }
+}
+
+inline fun <reified T : Parcelable> Intent.putParcelable(
+    value: T,
+    key: String = T::class.java.simpleName
+) {
+    putExtra(key, value)
+}
+
+inline fun <reified T : Parcelable> SavedStateHandle.getParcelable(
+    key: String = T::class.java.simpleName
+): T? {
+    return get<T>(key)
 }
