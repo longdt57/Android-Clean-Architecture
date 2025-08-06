0f5fdca9d1d12a4869cc926279540f1dd23024a4
logan <logan@castalk.com>
2025-08-06 11:58:15 +0700

    Add AppConfigurationProvider.kt

--- app/src/main/java/com/app/androidcompose/di/AppModule.kt
+++ app/src/main/java/com/app/androidcompose/di/AppModule.kt
@@ -1,11 +1,13 @@
 package com.app.androidcompose.di
 
 import android.content.Context
+import com.app.androidcompose.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import leegroup.module.core.util.AppConfigurationProvider
 import leegroup.module.core.util.DispatchersProvider
 import leegroup.module.core.util.DispatchersProviderImpl
 
@@ -18,4 +20,19 @@ class AppModule {
 
     @Provides
     fun provideDispatchersProvider(): DispatchersProvider = DispatchersProviderImpl
+
+    @Provides
+    fun provideAppConfigurationProvider(): AppConfigurationProvider = object : AppConfigurationProvider {
+        override val debug: Boolean
+            get() = BuildConfig.DEBUG
+        override val buildType: String
+            get() = BuildConfig.BUILD_TYPE
+        override val flavor: String
+            get() = BuildConfig.FLAVOR
+        override val versionCode: Int
+            get() = BuildConfig.VERSION_CODE
+        override val versionName: String
+            get() = BuildConfig.VERSION_NAME
+
+    }
 }
--- gituser/src/main/java/leegroup/module/sample/gituser/di/data/GitUserKtorHttpClientModule.kt
+++ gituser/src/main/java/leegroup/module/sample/gituser/di/data/GitUserKtorHttpClientModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
+import leegroup.module.core.util.AppConfigurationProvider
 import leegroup.module.data.provider.KtorHttpClientProvider
 import leegroup.module.sample.gituser.BuildConfig
 import javax.inject.Named
@@ -15,9 +16,11 @@ internal class GitUserKtorHttpClientModule {
 
     @Named(GIT_USER_KTOR_HTTP_CLIENT)
     @Provides
-    fun provideGitUserKtorHttpClient(): HttpClient {
+    fun provideGitUserKtorHttpClient(
+        appConfigurationProvider: AppConfigurationProvider
+    ): HttpClient {
         return KtorHttpClientProvider.provideHttpClient(
-            isLoggingEnable = BuildConfig.DEBUG,
+            isLoggingEnable = appConfigurationProvider.debug,
             configs = {},
             block = {
                 url(BuildConfig.BASE_API_URL)
