6531dd63872998b9733d022d99dad903e645c68c
Long Do <Longdt57@gmail.com>
2025-09-20 20:04:04 +0700

    Update collectAsEffect

--- core/core-ktx/src/main/java/leegroup/module/core/extensions/compose/FlowExt.kt
+++ core/core-ktx/src/main/java/leegroup/module/core/extensions/compose/FlowExt.kt
@@ -3,20 +3,24 @@ package leegroup.module.core.extensions.compose
 import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.withContext
 
 @SuppressLint("ComposableNaming")
 @Composable
 fun <T> Flow<T>.collectAsEffect(
-    context: CoroutineContext = EmptyCoroutineContext,
-    block: suspend (T) -> Unit,
+    onEvent: suspend (T) -> Unit,
 ) {
-    LaunchedEffect(key1 = Unit) {
-        onEach(block).flowOn(context).launchIn(this)
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(this, lifecycleOwner) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                collect(onEvent)
+            }
+        }
     }
 }
